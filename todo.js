a1b211cbc24d169f677cf084ef70f6760b3bce7a
syamjith <skasonline@gmail.com>
2021-09-25 14:30:47 +0530

    2

--- todo.js
+++ todo.js
@@ -7,7 +7,17 @@ function load(){
             if(this.readyState==4&&this.status==200){
              response =JSON.parse(this.responseText);
         
-            calldata()
+             var list="";
+             for (let i = 0; i <response.length; i++) {
+                 if(response[i].completed===true){
+                 list += `<li><input  type="checkbox" checked disabled><label  class="strikethrough" >${response[i].title}</label></li>`;
+                 disabled += 1 
+                     
+             }else{
+                 list += `<li><input  type="checkbox" onchange="check()"><label  class="strikethrough" >${response[i].title}</label></li>`;
+             }
+          }
+          document.getElementById("list").innerHTML= list;
               }
         }
         xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
@@ -15,26 +25,18 @@ function load(){
 }
 var disabled=0
 
-function calldata(){    
-    var list="";
-    for (let i = 0; i <response.length; i++) {
-        if(response[i].completed===true){
-        list += `<li>${response[i].title}<input class="check" type="checkbox"  checked disabled></li>`;
-        disabled += 1 
-            
-    }else{
-        list += `<li>${response[i].title}<input class="check" type="checkbox" onchange="check()"></li>`;
-    }
- }
- document.getElementById("list").innerHTML= list;
-}  
 
 
 
 function check(){
+   
+
         var promise = new Promise(function(resolve,reject){
+        
+    
        
          var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked ');
+
        
         var checked = checkboxes.length-disabled;
         if(checked==5){
